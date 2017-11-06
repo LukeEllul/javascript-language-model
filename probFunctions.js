ee40aec9aa833217db0c741e30effa158c0a574e
LukeEllul <ellul.luke@gmail.com>
2017-11-07 00:58:54 +0100

    added extra functionality

--- probFunctions.js
+++ probFunctions.js
@@ -1,21 +1,40 @@
 const R = require('ramda');
 const { Map, List } = require('immutable');
-const fs = require('fs');
+const { text } = require('./text');
 
 const product = (...N) => N.reduce((num, n) => num * n);
 
 const cond = (A, ...B) => P => P(...B, A) / P(...B);
 
+const iterateTokens = n => (...tokens) => (P, fn) =>
+    tokens.map(
+        (t, i) => {
+            const slicedTokens = n !== 1 && tokens.slice(
+                (j => j < 0 || n === 0 ? 0 : j)(i - (n - 1)), i);
+            return (fn || (v => v))
+                (n === 1 || i === 0 ? P(t) :
+                    cond(t, ...slicedTokens)(P), t, slicedTokens)
+        });
+
 const markov = n => (...tokens) => P =>
-    product(...tokens.map(
-        (t, i) => n === 1 || i === 0 ? P(t) :
-            cond(t, ...tokens.slice(
-                (j => j < 0 || n === 0 ? 0 : j)(i - (n - 1)), i))(P)
-    ));
+    product(...iterateTokens(n)(...tokens)(P));
 
 const chain = markov(0);
-const unigram = markov(1);
-const bigram = markov(2);
 
-const count = (...tokens) => console.log(tokens) ||
-    (text.match(new RegExp(tokens.join(' '), 'gi')) || []).length / textNum;
+const generateCount = text => {
+    const textNum = text.split(' ').length;
+    return (...tokens) => 
+        (text.match(new RegExp(tokens.join(' '), 'gi')) || []).length / textNum;
+};
+
+const v = iterateTokens(2)(...text.split(' '))(
+    generateCount(text),
+    (v, t, historyTokens) => ({ [t]: [v, historyTokens.toString()] })
+);
+
+console.log(v);
+
+module.exports = {
+    markov,
+    chain
+}
---
+++ text.js
@@ -0,0 +1,20 @@
+const text = `
+    Everything about Sybil Stallone is larger than life! She has got 
+    big tits, a big booty, big lips, and an even bigger libido! 
+    This bodacious babe loves dick so much that she has to travel 
+    around the globe to get her fill. A true jetsetter, Sybil is a 
+    citizen of the world, flying from Japan to Indonesia to Vegas 
+    and Miami at the drop of a hat. But wherever she goes, Miss 
+    Stallone does it in style, drinking the finest champagne, 
+    wearing only the best designer fashions, and relaxing in five 
+    star hotels. All that luxury might seem excessive, but this 
+    lovely lady deserves ever last penny, because her big juicy 
+    ass works hard! Confident, powerful, and independent, Sybil 
+    always practices self-love, and what better way to pay for her 
+    first-class lifestyle than to pleasure herself in every corner 
+    of the internet? Sybil Stallone is the best!
+`;
+
+module.exports = {
+    text
+};

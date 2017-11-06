3f87068d0fcf52c74ba3590d098a5ffbb666e76e
LukeEllul <ellul.luke@gmail.com>
2017-11-06 21:31:26 +0100

    changed chain function into markov function and added count function to lm.js

--- lm.js
+++ lm.js
@@ -1,13 +1,21 @@
 const R = require('ramda');
 const { Map, List } = require('immutable');
 const fs = require('fs');
-const { } = require("./text");
 
 const product = (...N) => N.reduce((num, n) => num * n);
 
 const cond = (A, ...B) => P => P(...B, A) / P(...B);
 
-const chain = (...ch) => P =>
-    product(...ch.map(
-        (v, i) => i === 0 ? P(v) : cond(v, ...ch.slice(0, i))(P)
+const markov = n => (...tokens) => P =>
+    product(...tokens.map(
+        (t, i) => n === 1 || i === 0 ? P(t) :
+            cond(t, ...tokens.slice(
+                (j => j < 0 || n === 0 ? 0 : j)(i - (n - 1)), i))(P)
     ));
+
+const chain = markov(0);
+const unigram = markov(1);
+const bigram = markov(2);
+
+const count = (...tokens) => console.log(tokens) ||
+    (text.match(new RegExp(tokens.join(' '), 'gi')) || []).length / textNum;

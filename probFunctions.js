57845e86d5aa5c589e4a331d5b55e02da076a159
LukeEllul <ellul.luke@gmail.com>
2017-11-09 22:05:34 +0100

    added support for laplace smoothing in probFunctions.js

--- probFunctions.js
+++ probFunctions.js
@@ -3,16 +3,17 @@ const { Map, List } = require('immutable');
 
 const product = (...N) => N.reduce((num, n) => num * n);
 
-const cond = (A, ...B) => P => P(...B, A) / P(...B);
+const cond = (A, ...B) => (P, V) => 
+    (P(...B, A) + (V ? 1 : 0)) / (P(...B) + (V ? V : 0));
 
-const iterateTokens = n => (...tokens) => (P, fn) =>
+const iterateTokens = n => (...tokens) => (P, fn, V) =>
     tokens.map(
         (t, i) => {
             const slicedTokens = n !== 1 && tokens.slice(
                 (j => j < 0 || n === 0 ? 0 : j)(i - (n - 1)), i);
             return (fn || (v => v))
                 (
-                    n === 1 || i === 0 ? P(t) : cond(t, ...slicedTokens)(P),
+                    n === 1 || i === 0 ? P(t) : cond(t, ...slicedTokens)(P, V),
                     t,
                     slicedTokens,
                     P(t)

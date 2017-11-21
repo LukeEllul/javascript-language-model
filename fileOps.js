936ede71e26837f0e75d3816c6a4a7239d347e44
Luke Ellul <ellul.luke@gmail.com>
2017-11-21 21:30:13 +0100

    modified iterateThroughFiles function to work asynchronously

--- fileOps.js
+++ fileOps.js
@@ -1,10 +1,15 @@
 const fs = require('fs');
 
-const iterateThroughFiles = locations => cb => 
-    locations.reduce((promise, location) =>
-        promise.then(() => new Promise((res, rej) => 
-            fs.readFile(location, data => res(cb(data))))),
-        Promise.resolve());
+// const iterateThroughFiles = locations => cb => 
+//     locations.reduce((promise, location) =>
+//         promise.then(() => new Promise((res, rej) => 
+//             fs.readFile(location, (err, data) =>
+//                 err ? rej(err) : res(cb(data))))),
+//         Promise.resolve());
+
+const iterateThroughFiles = (locations, origLocs = locations) => cb =>
+    locations.length === 0 ? origLocs :
+    (cb(fs.readFileSync(locations[0])), iterateThroughFiles(locations.slice(1), origLocs));
 
 module.exports = {
     iterateThroughFiles
--- tokens.js
+++ tokens.js
@@ -1,7 +1,7 @@
 const R = require('ramda');
 const {bigram, trigram, laplace} = require('./processing');
 const { text } = require('./text');
-const fs = require('fs');
+const {iterateThroughFiles} = require('./fileOps');
 
 const matchWords = text => text.match(/\w+/gi);
 
@@ -15,6 +15,13 @@ const removePunctuation = text => joinTokens(...matchWords(text));
 const getSentences = text =>
     (text.match(/[A-Z\d][^.!?]*[.!?]/g) || [])
         .map(removePunctuation);
+
+const getWordCountOfTexts = textLocations => {
+    let count = 0;
+    return iterateThroughFiles(textLocations)(
+        text => count = count + getWordCount(text.toString())
+    );
+}
 
 const generateCountFn = text => {
     //todo: rearrange generateCountFn to handle extra large files of
